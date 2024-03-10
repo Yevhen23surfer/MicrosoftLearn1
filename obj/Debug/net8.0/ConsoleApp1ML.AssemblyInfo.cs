68e43b6c90836f26dfacc6406da1b94383c452e4
Yevhen Surfer <23skopets@gmail.com>
2024-03-10 21:16:57 +0100

    new challenge about subscription

--- obj/Debug/net8.0/ConsoleApp1ML.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp1ML.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1ML")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22338202117195f8379a84036f5fd3bec43aa5ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a85d33c15d605b1ec0e9ed3cae13f6ca1e93aab4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1ML")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1ML")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
