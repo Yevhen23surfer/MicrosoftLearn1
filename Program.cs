4406c6c986d94d2b6f6a02df9fc54dd41c5d681a
Yevhen Surfer <23skopets@gmail.com>
2024-03-12 14:26:13 +0100

    dice

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
-﻿string [] OrderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
-
-foreach (string OrderID in OrderIDs)
-{
-    if (OrderID.StartsWith("B"))
-    Console.WriteLine(OrderID);
+﻿Random dice = new Random ();
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+int total = roll1 + roll2 + roll3;
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) {
+    if ((roll1 == roll2) && (roll2 == roll3)) {
+        Console.WriteLine("You rolled triples! +6 bonus to total!");
+        total += 6;
+    } else {
+        Console.WriteLine("You rolled doubles! +2 bonus to total!");
+        total += 2;
+    }
 }
--- obj/Debug/net8.0/ConsoleApp1ML.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp1ML.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1ML")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1941c5627b7b1df448f552830077cdafc0df1c72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77380147c01a8605904e263198bbda32ddf7dbc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1ML")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1ML")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
