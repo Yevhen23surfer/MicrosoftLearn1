b65c574d879ca07c9f8c7d9fd2fc595981655320
Yevhen Surfer <23skopets@gmail.com>
2024-04-07 17:23:02 +0200

    Floating point types

--- obj/Debug/net8.0/ConsoleApp1ML.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp1ML.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1ML")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88ac6b5bd4c5d902dc5bdfbe3886d9dfcce6cf7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df9c92256020a5b8ab264c3006ae1bcd9313dad9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1ML")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1ML")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
