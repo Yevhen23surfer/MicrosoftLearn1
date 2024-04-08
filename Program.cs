52065b691b8889736f4b3318a7b79f4176487fc3
Yevhen Surfer <23skopets@gmail.com>
2024-04-08 10:29:51 +0200

    Use ToString() to convert a number to a string

--- Program.cs
+++ Program.cs
@@ -39,8 +39,15 @@
 // int myInt = (int)myDecimal;
 // Console.WriteLine($"int: {myInt}");
 
-decimal myDecimal = 1.23456789m;
-float myFloat = (float)myDecimal;
 
-Console.WriteLine($"Decimal: {myDecimal}");
-Console.WriteLine($"Float  : {myFloat}");
+// decimal myDecimal = 1.23456789m;
+// float myFloat = (float)myDecimal;
+
+// Console.WriteLine($"Decimal: {myDecimal}");
+// Console.WriteLine($"Float  : {myFloat}");
+
+
+int first = 5;
+int second = 7;
+string message = first.ToString() + second.ToString();
+Console.WriteLine(message);
--- obj/Debug/net8.0/ConsoleApp1ML.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp1ML.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1ML")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b65c574d879ca07c9f8c7d9fd2fc595981655320")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7f199077e7391514b222647b7bb91ebea027df1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1ML")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1ML")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
