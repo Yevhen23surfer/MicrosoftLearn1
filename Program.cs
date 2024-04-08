3c407c6f4c68404618f9279485f23de95276d317
Yevhen Surfer <23skopets@gmail.com>
2024-04-08 12:25:54 +0200

    TryParse() a string into an int

--- Program.cs
+++ Program.cs
@@ -65,8 +65,20 @@
 // Console.WriteLine(result);
 
 
-int value = (int)1.5m; // casting truncates
-Console.WriteLine(value);
+// int value = (int)1.5m; // casting truncates
+// Console.WriteLine(value);
 
-int value2 = Convert.ToInt32(1.5m); // converting rounds up
-Console.WriteLine(value2);
+// int value2 = Convert.ToInt32(1.5m); // converting rounds up
+// Console.WriteLine(value2);
+
+
+string value = "102";
+int result = 0;
+if (int.TryParse(value, out result))
+{
+    Console.WriteLine($"Measurement: {result}");
+}
+else
+{
+    Console.WriteLine("Unable to report the measurement.");
+}
