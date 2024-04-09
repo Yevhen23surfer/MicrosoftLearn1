7436dc2bba7a691ea2028710aa1b90d8a3312547
Yevhen Surfer <23skopets@gmail.com>
2024-04-09 11:21:40 +0200

    Access the value of a cleared element

--- Program.cs
+++ Program.cs
@@ -2,11 +2,15 @@
 // - Work with Variable Data in C# Console Applications
 // - - Module: Perform Operations on Arrays Using Helper Methods in C#
 
-// Use array methods to clear and resize an array
+// Access the value of a cleared element
+
 string[] pallets = { "B14", "A11", "B12", "A13" };
 Console.WriteLine("");
 
+Console.WriteLine($"Before: {pallets[0]}");
 Array.Clear(pallets, 0, 2);
+Console.WriteLine($"After: {pallets[0]}");
+
 Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
 foreach (var pallet in pallets)
 {
--- obj/Debug/net8.0/ConsoleApp1ML.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp1ML.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1ML")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab2ece26f8f63a806373441cdb418fd36f861efe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a10c80a8abba1900268e2b16c15a065206b1e068")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1ML")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1ML")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
