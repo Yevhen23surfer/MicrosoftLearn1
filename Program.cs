4aa0ce4f689d4c075774e9ecae2ff477fe068608
Yevhen Surfer <23skopets@gmail.com>
2024-04-09 13:46:46 +0200

    Complete a challenge to reverse words in a sentence

--- Program.cs
+++ Program.cs
@@ -2,17 +2,31 @@
 // - Work with Variable Data in C# Console Applications
 // - - Module: Perform Operations on Arrays Using Helper Methods in C#
 
-// Exercise - Discover Split() and Join()
+// Exercise - Complete a challenge to reverse words in a sentence
 
-string value = "abc123";
-char[] valueArray = value.ToCharArray();
-Array.Reverse(valueArray);
-// string result = new string(valueArray);
-string result = String.Join(",", valueArray);
-Console.WriteLine(result);
+// string value = "abc123";
+// char[] valueArray = value.ToCharArray();
+// Array.Reverse(valueArray);
+// // string result = new string(valueArray);
+// string result = String.Join(",", valueArray);
+// Console.WriteLine(result);
 
-string[] items = result.Split(',');
-foreach (string item in items)
+// string[] items = result.Split(',');
+// foreach (string item in items)
+// {
+//     Console.WriteLine(item);
+// }
+
+string pangram = "the quick brown fox jumpes over the lazy dog";
+string[] message = pangram.Split(' ');
+string[] newMessage = new string[message.Length];
+
+for (int i = 0; i < message.Length; i++)
 {
-    Console.WriteLine(item);
+    char[] letters = message[i].ToCharArray();
+    Array.Reverse(letters);
+    newMessage[i] = new string(letters);    
 }
+
+string result = String.Join(" ", newMessage);
+Console.WriteLine(result);
--- obj/Debug/net8.0/ConsoleApp1ML.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp1ML.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1ML")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4dedaf879321e0243916cbbbd3d44466d7a6a65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2152ec69be652d9324c2d93541679b606cd7d691")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1ML")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1ML")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
