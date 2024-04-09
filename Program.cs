1c9e5cf34a02bf2d8ed0ba04c9cb30ad1d07580f
Yevhen Surfer <23skopets@gmail.com>
2024-04-09 11:00:27 +0200

    Create an arrey of pallets, then sort them

--- Program.cs
+++ Program.cs
@@ -1,121 +1,13 @@
-﻿// Console.WriteLine("Signed integral types:");
+﻿// Foundational C# with Microsoft
+// - Work with Variable Data in C# Console Applications
+// - - Module: Perform Operations on Arrays Using Helper Methods in C#
 
-// Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
-// Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
-// Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
-// Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
+// Create an arrey of pallets, then sort them
+string[] pallets = { "B14", "A11", "B12", "A13" };
 
-// Console.WriteLine("");
-// Console.WriteLine("Unsigned integral types:");
-
-// Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
-// Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
-// Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
-// Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
-
-// Console.WriteLine("");
-// Console.WriteLine("Floating point types:");
-// Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digis of precision)");
-// Console.WriteLine($"double : {double.MinValue} to {float.MaxValue} (with ~15-17 digis of precision)");
-// Console.WriteLine($"decimal : {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digis of precision)");
-
-
-// int[] data = new int[3];
-
-// string shortenedString = "Hello World!";
-// Console.WriteLine(shortenedString);
-
-
-// int myInt = 3;
-// Console.WriteLine($"int: {myInt}");
-
-// decimal myDecimal = myInt;
-// Console.WriteLine($"decimal: {myDecimal}");
-
-
-// decimal myDecimal = 3.14m;
-// Console.WriteLine($"decimal: {myDecimal}");
-
-// int myInt = (int)myDecimal;
-// Console.WriteLine($"int: {myInt}");
-
-
-// decimal myDecimal = 1.23456789m;
-// float myFloat = (float)myDecimal;
-
-// Console.WriteLine($"Decimal: {myDecimal}");
-// Console.WriteLine($"Float  : {myFloat}");
-
-
-// int first = 5;
-// int second = 7;
-// string message = first.ToString() + second.ToString();
-// Console.WriteLine(message);
-
-
-// string first = "5";
-// string second = "7";
-// int sum = int.Parse(first) + int.Parse(second);
-// Console.WriteLine(sum);
-
-
-// string value1 = "5";
-// string value2 = "7";
-// int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
-// Console.WriteLine(result);
-
-
-// int value = (int)1.5m; // casting truncates
-// Console.WriteLine(value);
-
-// int value2 = Convert.ToInt32(1.5m); // converting rounds up
-// Console.WriteLine(value2);
-
-
-// string value = "bad";
-// int result = 0;
-// if (int.TryParse(value, out result))
-// {
-//     Console.WriteLine($"Measurement: {result}");
-// }
-// else
-// {
-//     Console.WriteLine("Unable to report the measurement.");
-// }
-// if (result > 0)
-//     Console.WriteLine($"Measurement (w/ offset): {50 + result}");
-
-// // EXERCISE 1
-// string[] values = { "12.3", "45", "ABC", "11", "DEF" };
-// string message = "";
-// decimal total = 0m;
-
-// foreach (var value in values)
-// {
-//     decimal number;
-//     if (decimal.TryParse(value, out number))
-//     {
-//         total += number;
-//     } else
-//     {
-//         message += value;
-//     }
-// }
-
-// Console.WriteLine("Message: " + message);
-// Console.WriteLine("Total: " + total);
-
-// Exercise 2
-int value1 = 12;
-decimal value2 = 6.2m;
-float value3 = 4.3f;
-
-int result1 = Convert.ToInt32(value1 / value2);
-Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
-
-decimal result2 = value2 / (decimal)value3;
-Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
-
-float result3 = value3 / value1;
-Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
-
+Console.WriteLine("Sorted...");
+Array.Sort(pallets);
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
