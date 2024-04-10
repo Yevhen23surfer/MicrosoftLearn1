f7a6766e45216266193c13b9530b87f4f005e136
Yevhen Surfer <23skopets@gmail.com>
2024-04-10 10:40:32 +0200

    start module: Format alphanumeric data for presentation in C#

--- Program.cs
+++ Program.cs
@@ -1,51 +1,8 @@
 ﻿// Foundational C# with Microsoft
 // - Work with Variable Data in C# Console Applications
-// - - Module: Perform Operations on Arrays Using Helper Methods in C#
+// - - Format alphanumeric data for presentation in C#
 
-
-// string value = "abc123";
-// char[] valueArray = value.ToCharArray();
-// Array.Reverse(valueArray);
-// // string result = new string(valueArray);
-// string result = String.Join(",", valueArray);
-// Console.WriteLine(result);
-
-// string[] items = result.Split(',');
-// foreach (string item in items)
-// {
-//     Console.WriteLine(item);
-// }
-
-// string pangram = "the quick brown fox jumpes over the lazy dog";
-// string[] message = pangram.Split(' ');
-// string[] newMessage = new string[message.Length];
-
-// for (int i = 0; i < message.Length; i++)
-// {
-//     char[] letters = message[i].ToCharArray();
-//     Array.Reverse(letters);
-//     newMessage[i] = new string(letters);    
-// }
-
-// string result = String.Join(" ", newMessage);
-// Console.WriteLine(result);
-
-
-
-// Exercise - Complete a challenge to parse a string of orders, sort the orders and tag possible errors
-
-string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
-string[] items = orderStream.Split(',');
-Array.Sort(items);
-
-foreach (var item in items)
-{
-    if (item.Length == 4)
-    {
-        Console.WriteLine(item);
-    }
-    else
-    {
-        Console.WriteLine(item + "\t- Error");
-    }
-}
+string first = "Hello";
+string second = "World";
+Console.WriteLine("{1} {0}", first, second);
+Console.WriteLine("{0} {0} {0}", first, second);
