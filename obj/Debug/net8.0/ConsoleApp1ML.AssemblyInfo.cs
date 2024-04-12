40b893495d544975221d917894cc448fa1848e57
Yevhen Surfer <23skopets@gmail.com>
2024-04-12 12:48:25 +0200

    done

--- obj/Debug/net8.0/ConsoleApp1ML.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp1ML.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1ML")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d5aba44011b0a4f51739acd3ff48880b45ce5f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c0b4434937d04926ccaf84f535d1b70370abfd2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1ML")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1ML")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
