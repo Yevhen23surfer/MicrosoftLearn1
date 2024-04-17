883da3fe8f854610502a0c1ae890cac2044196b5
Yevhen Surfer <23skopets@gmail.com>
2024-04-17 11:20:22 +0200

    Create a method to display random numbers

--- Program.cs
+++ Program.cs
@@ -1,39 +1,18 @@
 ﻿// Foundational C# with Microsoft
-// - Work with Variable Data in C# Console Applications
-// - - Modify the Content of Strings Using Built-In String Data Type Methods in C#
+// - Write your first C# method 
+// - - Exercise - Create your first method
 
+Console.WriteLine("Generating random numbers:");
+DisplayRandomNumbers();
 
-const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+void DisplayRandomNumbers() 
+{
+    Random random = new Random();
 
-string quantity = "";
-string output = "";
+    for (int i = 0; i < 5; i++) 
+    {
+        Console.Write($"{random.Next(1, 100)} ");
+    }
 
-// Your work here
-
-// Extract the "quantity"
-const string openSpan = "<span>";
-const string closeSpan = "</span>";
-
-int quantityStart = input.IndexOf(openSpan) + openSpan.Length;
-int quantityEnd = input.IndexOf(closeSpan);
-int quantityLength = quantityEnd - quantityStart;
-quantity = input.Substring(quantityStart, quantityLength);
-quantity = $"Quantity: {quantity}";
-
-// Set output to input, replacing the trademark symbol with the registered trademark symbol
-const string tradeSymbol = "&trade;";
-const string regSymbol = "&reg;";
-output = input.Replace(tradeSymbol, regSymbol);
-
-// Remove the opening <div> tag
-const string openDiv = "<div>";
-int divStart = output.IndexOf(openDiv);
-output = output.Remove(divStart, openDiv.Length);
-
-// Remove the closeing </div> tag and add "Output:" to the beginning
-const string closeDiv = "</div>";
-int divCloseStart = output.IndexOf(closeDiv);
-output = "Output: " + output.Remove(divCloseStart, closeDiv.Length);
-
-Console.WriteLine(quantity);
-Console.WriteLine(output);
+    Console.WriteLine();
+}
