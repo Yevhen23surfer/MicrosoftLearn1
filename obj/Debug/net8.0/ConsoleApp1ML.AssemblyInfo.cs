d8e842f7b05dd699655e03fc8e9740f581ee5d3d
Yevhen Surfer <23skopets@gmail.com>
2024-04-27 19:35:24 +0200

    Exercise - Return arrays from methods / Finding multiple pairs of coins that make a difference

--- obj/Debug/net8.0/ConsoleApp1ML.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp1ML.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1ML")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38c9f45a768099dfa5c7941a11d04ffcdc6464fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43edfc5c60237639eb55673ae77f7916183a0dce")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1ML")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1ML")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
